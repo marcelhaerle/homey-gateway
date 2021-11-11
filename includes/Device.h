7892247cf5b52b589849126251e3ac14210da5eb
Marcel Härle <marcel.haerle@sonnvest.de>
2021-11-11 16:07:39 +0100

    Cleanup code

--- includes/Device.h
+++ includes/Device.h
@@ -7,19 +7,24 @@
 class Device {
 private:
     std::string m_mac;
-    gatt_connection_t* m_connection;
-    std::vector<uint8_t> readUUID(std::string uuid);
-    std::string readString(std::string uuid);
-    float readFloat(std::string uuid);
-    int readInt(std::string uuid);
+    gatt_connection_t* m_connection = nullptr;
+    std::vector<uint8_t> readUUID(const std::string& uuid) const;
+    std::string readString(const std::string& uuid) const;
+    float readFloat(const std::string& uuid) const;
+    int readInt(const std::string& uuid) const;
+    const std::string DEVICE_ID_UUID   = "00000000-0000-0000-0000-000000000001";
+    const std::string TEMPERATURE_UUID = "00000000-0000-0000-0000-000000000002";
+    const std::string HUMIDITY_UUID    = "00000000-0000-0000-0000-000000000003";
+    const std::string ECO2_UUID        = "00000000-0000-0000-0000-000000000004";
+    const std::string TVOC_UUID        = "00000000-0000-0000-0000-000000000005";
 
 public:
-    Device(std::string mac);
+    explicit Device(std::string mac);
     bool connect();
     bool disconnect();
-    std::string readDeviceId();
-    float readTemperature();
-    float readHumidity();
-    int readEco2();
-    int readTvoc();
+    std::string readDeviceId() const;
+    float readTemperature() const;
+    float readHumidity() const;
+    int readEco2() const;
+    int readTvoc() const;
 };
